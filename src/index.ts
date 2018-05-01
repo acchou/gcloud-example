f02add7faa5b9472cc613e8a4d75228b4c182b07
Andy Chou <acchou2@gmail.com>
2018-04-30 17:52:00 -0700

    fix compute example, getting a minimal response

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import Storage = require("@google-cloud/storage");
-
 import { google } from "googleapis";
+import humanStringify from "human-stringify";
 
 const compute = google.compute("v1");
 
 //const zone = compute.zone("us-west1-a");
 const vmName = "derivative-instance-1";
 
-const storage = Storage();
+const zone = "us-west1-a";
 
 async function main() {
     try {
-        // const instances = await compute.instances.();
+        const auth = await google.auth.getClient({
+            scopes: ["https://www.googleapis.com/auth/compute"]
+        });
 
-        const [bucket] = await storage.createBucket("foo");
-        console.log(`created bucket ${bucket}`);
+        const project = await google.auth.getDefaultProjectId();
+
+        const response = await compute.instances.list({ auth, project, zone });
+        console.log(`Instances list response: ${humanStringify(response)}`);
+        for (const instance of response.data.items) {
+            console.log(instance);
+        }
     } catch (err) {
         console.warn(`EXCEPTION: `);
         console.warn(err);
